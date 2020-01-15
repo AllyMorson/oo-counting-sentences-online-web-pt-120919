bf78c7b45734e502b18128c54b335bfcf2cc1c1a
Ally Morson <ally.morson@gmail.com>
2020-01-15 18:45:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
+    require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #split initial open and final close is with "/ " then opens with \ &c closes with ' | ' for what is being split
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
+
+
